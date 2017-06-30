785619a7536fa35f2a192cbdaf888ebc69a38b07
André Brandão <andrenbrandao@gmail.com>
2017-06-29 21:22:06 -0300

    Modify PLI to use vertices and edges

--- prize_collecting_with_edges_BAK.cpp
+++ prize_collecting_with_edges_BAK.cpp
@@ -20,13 +20,12 @@ class ConnectivityCuts: public GRBCallback
     ListDigraph::Node s;
     ListDigraph::Node t;
     std::vector<ListDigraph::Node> &path;
-    ListDigraph::ArcMap<GRBVar>& xa;
-    ListDigraph::NodeMap<GRBVar>& xv;
+    ListDigraph::ArcMap<GRBVar>& x;
     double (GRBCallback::*solution_value)(GRBVar);
   public:
     ConnectivityCuts(ListDigraph& g, ListDigraph::NodeMap<double>& prize, ListDigraph::ArcMap<double>& cost, ListDigraph::Node s,
       ListDigraph::Node t, std::vector<ListDigraph::Node> &path,
-      ListDigraph::ArcMap<GRBVar>& xa,ListDigraph::NodeMap<GRBVar>& xv) : g(g), prize(prize), cost(cost), s(s), t(t), path(path), xa(xa), xv(xv)
+      ListDigraph::ArcMap<GRBVar>& x) : g(g), prize(prize), cost(cost), s(s), t(t), path(path), x(x)
     {    }
   protected:
     void callback()
@@ -40,23 +39,29 @@ class ConnectivityCuts: public GRBCallback
         DCutMap cut(g);
         double vcut;
         for (ArcIt a(g); a!=INVALID; ++a)
-          capacity[a] = (this->*solution_value)(xa[a]);  // or getSolution(x[a]);
+          capacity[a] = (this->*solution_value)(x[a]);  // or getSolution(x[a]);
 
         for (DNodeIt v(g); v!=INVALID; ++v) {
           if(v != s && v!= t) {
             GRBLinExpr expr;
             // find a mincut between root s and other terminal
-            // vcut = 1.0;
-            vcut = DiMinCut(g,capacity, s , v, cut);
+            vcut = 1.0;
+            for (ArcIt a(g); a!=INVALID; ++a) {
+              if(g.target(a) == v && getSolution(x[a]) >= 1.0-MY_EPS ) {
+                vcut = DiMinCut(g,capacity, s , v, cut);
+                break;
+              }
+            }
             if (vcut >= 1.0-MY_EPS) continue;
 
             // found violated cut
             for (ArcIt a(g); a!=INVALID; ++a)
-             if ((cut[g.source(a)]==cut[s]) && (cut[g.target(a)]!=cut[s])) {
-               expr += xa[a];
-             }
-             if(getSolution(xv[v]) >= 1.0-MY_EPS)
-                addLazy( expr >= 1.0  ); // or addLazy(expr,GRB_GREATER_EQUAL,1.0);
+             if ((cut[g.source(a)]==cut[s]) && (cut[g.target(a)]!=cut[s]))
+               expr += x[a];
+            // for(ArcIt a(g); a!=INVALID; ++a) {
+              // if(x[a] >= 1.0-MY_EPS )
+                addLazy( expr >= 1.0 ); // or addLazy(expr,GRB_GREATER_EQUAL,1.0);
+            // }
           }
         }
   } catch (GRBException e) {
@@ -68,6 +73,35 @@ class ConnectivityCuts: public GRBCallback
   }
 };
 
+void find_other_path(ListDigraph& g, ListDigraph::NodeMap<double>& prize, ListDigraph::ArcMap<double>& cost, ListDigraph::Node s, ListDigraph::Node t, std::vector<ListDigraph::Node> &path, double &LB, double &UB, int tMax, ListDigraph::ArcMap<GRBVar>& x) {
+  ListDigraph::Node node, last_node, new_node;
+
+  for (ArcIt e(g); e!=INVALID; ++e) {
+    if(BinaryIsOne(x[e].get(GRB_DoubleAttr_X))) {
+      if(find(path.begin(), path.end(), g.source(e)) != path.end()) {
+
+      }
+      else {
+        node = g.source(e);
+        break;
+      }
+    }
+  }
+
+  cout << g.id(node) << endl;
+  last_node = node;
+        while(last_node != INVALID) {
+          for ( OutArcIt e(g, last_node); e!=INVALID; ++e ){
+            if(BinaryIsOne(x[e].get(GRB_DoubleAttr_X))) {
+              new_node = g.target(e);
+              cout << g.id(new_node) << endl;
+              last_node = new_node;
+              // opt += prize[new_node] - cost[e];
+            }
+          }
+        }
+}
+
 ///
 // PLI function
 ///
@@ -77,8 +111,7 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
   GRBLinExpr expr;
   model.set(GRB_IntAttr_ModelSense, GRB_MAXIMIZE);
   model.set(GRB_DoubleParam_TimeLimit, tMax);
-  ListDigraph::ArcMap<GRBVar> xa(g); // variavel x indica se a aresta esta ou nao na solucao
-  ListDigraph::NodeMap<GRBVar> xv(g); // variavel x indica se a aresta esta ou nao na solucao
+  ListDigraph::ArcMap<GRBVar> x(g); // variavel x indica se a aresta esta ou nao na solucao
   ListDigraph::Node last_node, new_node; // nos utilizados para calcular o path
   double prize_value; // guarda o valor do premio
   double opt = 0.0;
@@ -86,11 +119,10 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
   // Adiciona uma variavel x para cada aresta
   // Nao utiliza os valores dos premios de s e de t
   for (ArcIt e(g); e!=INVALID; ++e) {
-    xa[e] = model.addVar(0.0, 1.0, -1.0 * cost[e],GRB_BINARY,"");
+    prize_value = prize[g.source(e)];
+    x[e] = model.addVar(0.0, 1.0, prize_value - 1.0 * cost[e],GRB_BINARY,"");
   }
-  for(DNodeIt v(g); v!=INVALID; ++v) {
-    xv[v] = model.addVar(0.0, 1.0, prize[v], GRB_BINARY, "");
-  }
+  model.addVar(1.0, 1.0, prize[t],GRB_BINARY,"");
   model.update();
 
   // para cada no diferente de s e t
@@ -101,10 +133,10 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
       GRBLinExpr out_edges = 0;
 
       for ( InArcIt e(g, v); e!=INVALID; ++e ){
-          in_edges += xa[e];
+          in_edges += x[e];
       }
        for ( OutArcIt e(g, v); e!=INVALID; ++e ){
-          out_edges += xa[e];
+          out_edges += x[e];
       }
       model.addConstr(in_edges - out_edges == 0);
     }
@@ -114,27 +146,25 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
   GRBLinExpr in_edges = 0;
   GRBLinExpr out_edges = 0;
   for ( InArcIt e(g, s); e!=INVALID; ++e ){
-    in_edges += xa[e];
+    in_edges += x[e];
   }
   for ( OutArcIt e(g, s); e!=INVALID; ++e ){
-    out_edges += xa[e];
+    out_edges += x[e];
   }
   model.addConstr(in_edges == 0);
   model.addConstr(out_edges == 1);
-  model.addConstr(xv[s] <= in_edges + out_edges);
 
   // Para o vertice t: uma aresta incide no no e nenhuma aresta sai dele
   in_edges = 0;
   out_edges = 0;
   for ( InArcIt e(g, t); e!=INVALID; ++e ){
-    in_edges += xa[e];
+    in_edges += x[e];
   }
   for ( OutArcIt e(g, t); e!=INVALID; ++e ){
-    out_edges += xa[e];
+    out_edges += x[e];
   }
   model.addConstr(in_edges == 1);
   model.addConstr(out_edges == 0);
-  model.addConstr(xv[t] <= in_edges + out_edges);
 
   // para cada no diferente de s e t
   // ate uma aresta sai e ate uma aresta entra
@@ -144,14 +174,13 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
       GRBLinExpr out_edges = 0;
 
       for ( InArcIt e(g, v); e!=INVALID; ++e ){
-          in_edges += xa[e];
+          in_edges += x[e];
       }
        for ( OutArcIt e(g, v); e!=INVALID; ++e ){
-          out_edges += xa[e];
+          out_edges += x[e];
       }
       model.addConstr(in_edges <= 1);
       model.addConstr(out_edges <= 1);
-      model.addConstr(xv[v] <= in_edges + out_edges);
     }
   }
 
@@ -163,7 +192,7 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
     // }
 
     model.set(GRB_IntParam_LazyConstraints, 1);
-    ConnectivityCuts cb = ConnectivityCuts(g, prize, cost, s, t, path, xa, xv);
+    ConnectivityCuts cb = ConnectivityCuts(g, prize, cost, s, t, path, x);
     model.setCallback(&cb);
 
     model.update();
@@ -183,7 +212,7 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
         last_node = s;
         while(last_node != t) {
           for ( OutArcIt e(g, last_node); e!=INVALID; ++e ){
-            if(BinaryIsOne(xa[e].get(GRB_DoubleAttr_X))) {
+            if(BinaryIsOne(x[e].get(GRB_DoubleAttr_X))) {
               new_node = g.target(e);
               path.push_back(new_node);
               last_node = new_node;
@@ -196,7 +225,7 @@ int prize_collecting_st_path_pli(ListDigraph& g, ListDigraph::NodeMap<double>& p
           int count = 0;
 
           for ( ArcIt e(g); e!=INVALID; ++e ){
-            if(BinaryIsOne(xa[e].get(GRB_DoubleAttr_X))) {
+            if(BinaryIsOne(x[e].get(GRB_DoubleAttr_X))) {
               cout << g.id(g.source(e)) << " ";
               count+=1;
             }
@@ -266,5 +295,5 @@ int prize_collecting_st_path_heuristic(ListDigraph& g, ListDigraph::NodeMap<doub
   // else
     LB =  -1 * (dijkstra.dist(t) - prize[s]);
 
-	return 0;
+  return 0;
 }
